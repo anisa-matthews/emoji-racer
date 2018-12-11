1071ac8a7e09434b089a4eb426fdda8bb1ebc2cb
Candace Johnson <ccj286@nyu.edu>
2018-12-11 17:50:39 -0500

    implemented connection

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const io = require('socket.io')(server);
 app.use(express.static('public'));
 // server code goes here!
 // first listen for connection using io.on
+io.on('connection', function(socket){
+    console.log('made a connection', socket.id);
+});
+
 // then... within callback, use socket.on, socket.emit, socket.broadcast, etc.
 // NOTE THAT WE ARE LISTENING WITH server, NOT app!
 server.listen(3000);
